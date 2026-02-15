0ec153b2a83c73af5270e22102ed05413f042f5f
msrtgit <msrt10.com@gmail.com>
2026-02-15 12:15:00 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for $instance in $@
 do
-    instance_id = $(aws ec2 run-instances \
+    instance_id=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
