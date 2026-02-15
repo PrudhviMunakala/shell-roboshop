9d630f9bf130f377d0198fc945157bcd6545de2c
msrtgit <msrt10.com@gmail.com>
2026-02-15 12:01:59 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -4,6 +4,15 @@ SG_ID="sg-02ea97487c317f700"
 AMI_ID="ami-0220d79f3f480ecf5"
 
 
-aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --security-group-ids sg-02ea97487c317f700 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=catalogue}]" --instance-type t3.micro
+for $INSTANCE in $@
+do
+aws ec2 run-instances \
+--image-id $AMI_ID \
+--security-group-ids $SG_ID \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
+--instance-type t3.micro \
+--query 'Instances[0].PrivateIpAddress' \
+--output text
+done
 
 
