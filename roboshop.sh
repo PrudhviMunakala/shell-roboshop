f04f0bf4adb2cfd30ab231d4d299ef64b296ca44
msrtgit <msrt10.com@gmail.com>
2026-02-15 12:13:02 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -4,15 +4,36 @@ SG_ID="sg-02ea97487c317f700"
 AMI_ID="ami-0220d79f3f480ecf5"
 
 
-for $INSTANCE in $@
+for $instance in $@
 do
-aws ec2 run-instances \
---image-id $AMI_ID \
---security-group-ids $SG_ID \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
---instance-type t3.micro \
---query 'Instances[0].PrivateIpAddress' \
---output text
+    instance_id = $(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --instance-type t3.micro \
+    --query 'Instances[0].PrivateIpAddress' \
+    --output text
+    )
+
+    if [ $instance_id == "frontedn"]; 
+    then
+       IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[].Instances[].PublicIpAddress' \
+        --output text
+        ) 
+    else 
+
+    IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[].Instances[].PrivateIpAddress' \
+        --output text
+        )
+    fi
+
 done
+
 
 
